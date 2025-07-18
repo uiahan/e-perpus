9bc79df8b0eb7d9c80229cff36484c9f20d0463f
uiahan <farhandikaalsani@gmail.com>
2025-07-18 15:37:08 +0700

    bug fixed

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [BookController::class, 'home'])->name('home');
@@ -8,4 +9,13 @@ Route::get('/books/search', [BookController::class, 'search'])->name('books.sear
 Route::get('/books/{id}', function ($id) {
     $book = App\Models\Book::with('categories')->findOrFail($id);
     return response()->json($book);
+});
+Route::get('/debug-auth', function () {
+    return [
+        'auth_web' => Auth::guard('web')->check(),
+        'auth_default' => Auth::check(),
+        'user' => Auth::user(),
+        'session' => session()->all(),
+        'cookie' => request()->cookie('laravel_session'),
+    ];
 });
