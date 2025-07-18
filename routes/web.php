9c4c189eebd1a6e058070458356ee8aec03639a5
uiahan <farhandikaalsani@gmail.com>
2025-07-18 16:08:39 +0700

    masih bug login

--- routes/web.php
+++ routes/web.php
@@ -10,22 +10,3 @@ Route::get('/books/{id}', function ($id) {
     $book = App\Models\Book::with('categories')->findOrFail($id);
     return response()->json($book);
 });
-Route::get('/force-login', function () {
-    $user = \App\Models\User::first();
-    Auth::login($user);
-    session()->regenerate();
-    return redirect('/debug-auth');
-});
-
-Route::get('/debug-auth', function () {
-    dd(Auth::user(), auth()->check(), session()->all());
-});
-Route::get('/test-session', function () {
-    session(['foo' => 'bar']);
-    return 'Session set';
-});
-
-Route::get('/get-session', function () {
-    return session('foo');
-});
-
